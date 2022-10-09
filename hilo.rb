cacca2e4227a94efcc07eb42e1fea1e74614397f
Katie French <katie.french@optimalworkshop.com>
2022-10-10 11:01:51 +1300

    Added message to let user know if they have guessed outside of the number range

--- hilo.rb
+++ hilo.rb
@@ -2,7 +2,7 @@
 
 gameWon = false
 lives = 6
-randomNum = rand(100) + 1
+randomNum = rand(1..100)
 
 puts "Welcome to a game of Ruby Hi-Lo 🤠"
 puts "To win this game correctly guess the random number between 1 and 100"
@@ -11,27 +11,34 @@ puts "Make a guess:"
 
 while !gameWon
   guessedNumber = gets.chomp.to_i
+  outOfRangeNum = guessedNumber < 1 || guessedNumber > 100
 
-  if guessedNumber > randomNum then
+  remainingLivesMessage = "You have #{lives} lives remaining. Guess again:"
+  gameOverMessage = "💀 💀 💀 Game lost. You have run out of lives 💀 💀 💀"
+
+  if guessedNumber > randomNum && !outOfRangeNum
     lives -= 1
 
-    if lives === 0 then
-      puts "💀 💀 💀 Game lost. You have run out of lives 💀 💀 💀"
+    if lives == 0
+      puts gameOverMessage
       break
     else
       puts "☹️ That number is too high - try again with a lower number 👇"
-      puts "You have #{lives} lives remaining. Guess again:"
+      puts remainingLivesMessage
     end
-  elsif guessedNumber < randomNum then
+  elsif guessedNumber < randomNum && !outOfRangeNum
     lives -= 1
 
-    if lives === 0 then
-      puts "💀 💀 💀 Game lost. You have run out of lives 💀 💀 💀"
+    if lives == 0
+      puts gameOverMessage
       break
     else
       puts "☹️ That number is too low - try again with a higher number 👆"
-      puts "You have #{lives} lives remaining. Guess again:"
+      puts remainingLivesMessage
     end
+  elsif outOfRangeNum
+    puts "Uh oh - you have guessed a number out of range."
+    puts "Make sure your guess is in the range of 1 - 100. Try again:"
   else
     puts "💫 ✨ Congratulations! You successfully guessed the correct number! ✨ 💫"
     gameWon = true
